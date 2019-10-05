4db97e898bd7e57a77c8abdff6356883aa606d84
1997prachi <prachi.princy97@gmail.com>
2019-10-06 01:34:21 +0530

    This is my second project

--- lecture1/even&odd.cpp
+++ lecture1/even&odd.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
-
 using namespace std;
-int main (int args , char **argv)
-{
-    int n=50;
-    for(i=1;)
- {
-    cout<<a*i<<" ";
-    i++;   
+
+int main(){
+    // / 5/2=3 5%2=1 6%2=0 7%2=1 10%5=0 12%11=1 11%12=11 10%35=10 12%2=0 13%2=1 14%2=0 15%2=1
+    
+    int n;
+    cin>>n; //user input
+    for(int i =1;i<=n;i++){
+if(i % 2== 1 ){
+
+    cout<<i<<"  : This number is odd"<<endl;
+   }
+else{
+cout<<i<<"  : This is Even number" << endl;
 }
-cout<<i<<endl;
+
+    }
+   
+
+    return 0;
 }
+
+//if(condiotn){
+    // if condtion true then this block will run 
+// }
+// else{
+
+// }
+
+
--- lecture1/table_a.cpp
+++ lecture1/table_a.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 
 int main(){
-    int a = 5;
-    for(int i=1;i<=10;){ // no semicolon will apply here and at if statements
+    // int a = 273;
+  //  int a = 5;
+   // for(int i=1;i<=10;){ // no semicolon will apply here and at if statements
+    int b = 112;
+    b =b+  273;
+    // b+=273; same thing as above 
 
-     cout<<i<< " "<<a<<" "<<i*a<<endl;
-     i++;
+    // b=7;
+ // updated value = 273 + previous value
+
+  //   cout<<i<< " "<<a<<" "<<i*a<<endl;
+  //   i++;
+ cout<<b<<" "<<endl;
  
- 
-    } // for loop ends here
+   // } // for loop ends here
     return 0;
 }
 
