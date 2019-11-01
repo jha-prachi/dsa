45114891a4a9f547b8a29e3a69fc06746d75573c
1997prachi <prachi.princy97@gmail.com>
2019-11-02 00:22:38 +0530

    bitquestions

---
+++ lecture9/bit_question.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class bit_question{
+public static Scanner scn =new Scanner(System.in);
+
+public static int noofBit_01(int n){
+    int count=0;
+            for(int i=0; i<32;i++)
+                if((n&(1<<i))!=0)     //mask =(1<<i);
+                    count++;
+    return count;
+  }
+    //    public static void 
+    public static void main (String[] args){
+        int n =scn.nextInt(); 
+       System.out.println(noofBit_01(n));
+    }
+}
+    
