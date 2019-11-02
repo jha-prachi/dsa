00ecd73a9154fc9d427be127cd6a8404eb0b8019
1997prachi <prachi.princy97@gmail.com>
2019-11-03 00:40:09 +0530

    another one

--- hackerrank/octal.cpp
+++ hackerrank/octal.cpp
@@ -39,10 +39,48 @@ int octaladd(int num1 , int num2){
 
  
     }
+
+
+
+    int octalsub(int num1 , int num2){
+    
+    if(num1<num2){
+   
+        int temp=num1;
+        num1 = num2 ;
+        num2 =temp;
+    }
+    int pow =1;
+    int res=0;
+    int borrow=0;
+    while(num1!=0){
+        int rem = num1 %10 - num2 %10 +borrow;
+        num1/=10;
+        num2/=10;
+        if(rem<0){
+            borrow =-1;
+            rem+=8;
+        }
+        else
+            borrow=0;
+        res+=rem*pow;
+        pow*=10;  
+    }
+ 
+    return res;
+
+    
+
+}
+
+
 int main() {
     int num1 , num2;
     cin>>num1>>num2;
-    cout<<octaladd(num1,num2);
+    // cout<<octaladd(num1,num2);
+
+    int val =octalsub( num1 , num2);
+    cout<<val;
     return 0;
 }
 
