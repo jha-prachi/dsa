8c373c49c6e27ac72d0afde4a1637f4123ea58cf
1997prachi <prachi.princy97@gmail.com>
2019-11-05 17:50:20 +0530

    arrayQ

---
+++ rough/array_ques.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+    void input(vector<vector<int>> &arr){
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr[0].size();j++){
+                cin>>arr[i][j];
+            } 
+        
+        }
+        
+    }
+    int display(vector<vector<int>> &arr){
+        for(int i =0;i<arr.size();i++){
+            for(int j =0;j<arr[0].size();j++){
+                cout<<arr[i][j]<<"  ";
+            }
+             cout<<endl;
+        }
+       
+    }
+    void wavemotion(vector<vector<int>> &arr){
+        for(int i=0;i<arr.size();i++){
+            if(i%2==0){
+            for(int j=0;j<arr[0].size();j++)
+            }
+            else{
+             for(j=arr[0].size();j>0;j--) 
+            }   
+        }
+    }
+    
+int main()
+{
+    int rows , column;
+    cin>>rows>>column;
+    vector<vector<int>> arr (rows,vector<int>(column,0));
+    input(arr);
+    display(arr);
+    return 0;
+}
+
+
+
+
+/*
+  0 1 2
+0
+1
+2
+
+vector 2D
+vector<vector<int>>arr(n ,vector<int>(m,0));
+java int[][] arr = new int[n][m];
+n -> row and m -> columns
+arr[][] 
+row size 
+arr.size() - cPP
+arr.length
+
+col size
+arr[0].size() - cpp
+arr[0].length
+
+arr[0] -> 1 2 3 4 5 - row0
+arr[1]->  6 7 8 9 10 - row1
+arr[2] -> 11 12 13 14 15 - row2
+arr[3]->  16 17 18 19 20 - row3
+arr[4] -> 21 22 23 24 25 - row4
+          | 
+       columns
+java 
+int arr[7]={4 ,5,6,78,98,45,12}
+vector<int> arr={3,4,4,5,5,6,6,8}
+
+Java 2D
+int  arr[][]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}}
+1 2 3
+4 5 6
+7 8 9
+10 11 12
+
+Cpp 2d
+vector<vector<int>> arr = {{1,2,3}}
+linear 
+1 2 3 4 5 10 9 8 7 6 11 12 13 1 15 20 19..
+
+
+(0,0) - 
+
+*/
