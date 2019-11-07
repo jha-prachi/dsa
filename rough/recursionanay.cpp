3af7ada9de0ae229c1569d5c11d5cf0fa967b308
1997prachi <prachi.princy97@gmail.com>
2019-11-08 01:46:13 +0530

    betterpower

--- rough/recursionanay.cpp
+++ rough/recursionanay.cpp
@@ -16,13 +16,23 @@ using namespace std;
      int ans=a*power(a , (b-1));
      return ans;
  }
+ int betterpower(int a , int b){
+     if(b==0){
+         return 1;
+     }
+     int ans  = betterpower(a , b/2);
+    ans*=ans;
+    return b%2==0?ans:ans*a;
 
+
+ }
 int main(){
-    cout<<power(2,8);
+    //cout<<power(2,8);
     //cout<<factoraial(5);
+    cout<<betterpower(2,9);
     return 0;
 }
-/* 
+/* ^ 12 
 a^b using recursion..
 int powerab(int a , int b ){
     a^5 = a * a^4
@@ -30,6 +40,22 @@ int powerab(int a , int b ){
     a^3 = a * a^2
     a^2 = a * a^1
     a^1 = a * a^0
+
+    a^9 = a^4
+    a^4 =a^2 * a^2
+    a^2 = a^1 * a^1
+    a^1 =a^0
+  
+    a^3 = a^1 * a^1 * a
+    betterpower(a , 9)
+    betterpowr(a,4)
+    betterpower(a , 2)
+    betterpower(a, 1)
+    betterpower(a, 0)
+    a^9 = a^4 
+
+
+
 }
 
 
