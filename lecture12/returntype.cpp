1f36ce68f1a7ee1e583235ab432218f904ae0f45
1997prachi <prachi.princy97@gmail.com>
2019-11-14 17:11:54 +0530

    removehiwithouthit

--- lecture12/returntype.cpp
+++ lecture12/returntype.cpp
@@ -24,8 +24,26 @@ string removeHi01(string ques){
 
 }
 
+string removehiwithouthit(string ques)
+{
+if(ques.length()==0)
+return " ";
+if(ques.length()>1 && ques[0]=='h' && ques[1]=='i')
+
+    if(ques.length()>2 && ques[2]=='t')
+        return "hit" + removehiwithouthit(ques.substr(3));
+else
+        return removehiwithouthit(ques.substr(2)); 
+
+else
+return ques[0]+removehiwithouthit(ques.substr(1));
+
+
+}
+
+
 int main(){
-    // removeHi("hjhisdhifhi","");
-    cout<<removeHi01("ihihhithithihhhhhithitt");
+   cout<<removehiwithouthit("ihihihithihihhithhhhithiiihithhithit");
+   // cout<<removeHi01("ihihhithithihhhhhithitt");
     return 0;
 }
