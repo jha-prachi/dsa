763b5d6fd7e333d852c786d74ce6a2197944ec7e
1997prachi <prachi.princy97@gmail.com>
2019-12-08 15:11:49 +0530

    Diagonal

--- hackerrank/pracQues.cpp
+++ hackerrank/pracQues.cpp
@@ -3,6 +3,7 @@
 #include<string>
 
 using namespace std;
+
 vector<string>Mazepath(int sr,int sc,int er,int ec)
 {
 if(sr==er&& sc==ec)
@@ -22,16 +23,23 @@ if(sc+1<=ec)
 {     myans.push_back("H"+ s);
 }
 }
+
  if(sr+1<=er){
      vector<string>Verti =Mazepath(sr+1,sc,er,ec);
      for(string s: Verti)
      {myans.push_back("V"+s);
      }
  }
+ if(sr<er  || sc<ec)
+{
+vector<string>Diagonal =Mazepath(sr+1,sc+1,er,ec);
+    for(string s:Diagonal)
+    myans.push_back("D"+s);
+}
  return myans;
 }
 int main(){
-vector<string> mp = Mazepath(0,0,2,2);
+vector<string> mp = Mazepath(0,0,3,3);
 for(string s : mp){
     cout<<s<< " ";
 }
