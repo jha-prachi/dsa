ad6c5d179049876f07bb1d0fc07dfc999ea1812f
1997prachi <prachi.princy97@gmail.com>
2019-12-08 15:38:19 +0530

    Permutation

--- hackerrank/pracQues.cpp
+++ hackerrank/pracQues.cpp
@@ -3,7 +3,28 @@
 #include<string>
 
 using namespace std;
-
+vector<string>Permutation(string ques){
+    if(ques.length()==0){
+        vector<string>base;
+        base.push_back("");
+        return base;
+    }
+   char ch=ques[0];
+     
+   vector<string>ans=Permutation(ques.substr(1));
+   vector<string>finalans;
+    
+   for(string str :ans)
+   {
+       for(int i=0;i<=str.length();i++){
+           string start =str.substr(0,i);
+           string end =str.substr(i);
+           string s=start+ch+end;
+           finalans.push_back(s);
+       }
+   } 
+   return finalans;
+}
 vector<string>Mazepath(int sr,int sc,int er,int ec)
 {
 if(sr==er&& sc==ec)
@@ -39,8 +60,11 @@ vector<string>Diagonal =Mazepath(sr+1,sc+1,er,ec);
  return myans;
 }
 int main(){
-vector<string> mp = Mazepath(0,0,3,3);
-for(string s : mp){
-    cout<<s<< " ";
-}
+vector<string>ans=Permutation("abc");
+for(string s:ans)
+    cout<<s<<" ";
+
+// vector<string> mp = Mazepath(0,0,3,3);
+// for(string s : mp){
+//     cout<<s<< " ";
 }
